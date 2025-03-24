11907794c52daf97e52fc0af2fe0671ef5c57d8d
dhushyandan <dhushyandan302002@gmail.com>
2025-03-24 18:26:08 +0530

    starting the project setuping the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+// App.jsx
+const App = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">DaisyUI + React</h1>
+      <button className="btn btn-primary mt-4">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
